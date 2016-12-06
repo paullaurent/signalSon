1b91698704f46782854fc3db49fec99f4aa847e7
Melanie Lim <mlim@ensc.fr>
2016-12-06 08:56:46 +0100

    code debut

---
+++ code/ProjetSonMain.m
@@ -0,0 +1,49 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% PROJET SIGNAL %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Question 1 
+
+N = 11025;
+f0 = 880;
+fe = 44100;
+N_v = 0:N-1; % vecteur 
+N_rep = 1000; %nombre d'échantillon voulu 
+
+ma_fct(fe, N, f0, N_rep);
+
+x = ma_fct(fe, N, f0, N_rep);
+
+soundsc(x,fe+2000); % si fe plus grand, son plus aigu
+
+
+
+%% 4 harmoniques
+nbh = 4;
+note = note_fct(N,f0,fe,nbh);
+ma_fct(note,fe,N,f0,N_rep);
+soundsc(note,fe);
+
+%% juste le fondamental
+note2 = note_fct(N,f0,fe,0);
+ma_fct(note2,fe,N,f0,N_rep);
+soundsc(note2,fe);
+
+%% Génération d'un morceau
+%liste_notes = {'C4', 'C4','C4','D4','E4','D4','C4','E4','D4','D4','C4','D4',...
+%    'D4','D4','D4','A3','B3','D4','C3','B3','A3','G3'} % accolade : cellule -> on peut mettre des éléments non homogènes
+
+liste_notes = {'F4','G4','A4','F4','F4','G4','A4','F4','A4','B4','C4','A4',...
+    'B4','C4','C4','D4','C4','B4','A4','F4','C4','D4','B4','A4','F4','F4','C3','F4','F4','C3','F4'}
+
+[tab_note] = tab_note_fct();
+taille_m = length(liste_notes);
+morceau = [];
+nb_h = 4;
+for ii = 1:taille_m
+    morceau=...
+        [morceau, zeros(1, floor(N/2)),...
+        eval(['note_fct(floor(N/2), tab_note.' liste_notes{ii} ',fe,nbh)'])]
+end
+
+soundsc(morceau,fe)
---
+++ code/tab_note_fct.m
@@ -0,0 +1,23 @@
+function [tab_note] = tab_note_fct()
+tab_note.C3 = 130.8;
+tab_note.C3d = 138.59;
+tab_note.D3 = 146.83;
+tab_note.D3d = 155.56;
+tab_note.E3 = 164.81;
+tab_note.F3 = 174.61;
+tab_note.F3d = 185.00;
+tab_note.G3 = 196.00;
+tab_note.G3d = 207.65;
+tab_note.A3 = 220.00;
+tab_note.A3d = 233.08;
+tab_note.B3 = 246.94;
+tab_note.C4 = 261.63;
+tab_note.C4d = 277.18;
+tab_note.D4 = 293.66;
+tab_note.D4d = 311.13;
+tab_note.E4 = 329.63;
+tab_note.F4 = 349.23;
+tab_note.G4 = 392.00;
+tab_note.A4 = 440.00;
+tab_note.B4 = 493.88;
+end
