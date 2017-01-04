19e765b7b71ee47e4232e5ec0dbb98ae5f9a9773
Melanie Lim <mlim@ensc.fr>
2017-01-04 16:13:33 +0100

    Etapes 4 et 5

--- code/blancGaussien.m
+++ code/blancGaussien.m
@@ -1,15 +1,23 @@
-function [signal]= blancGaussien(signal,RSB,type)
+function [signal]= blancGaussien(signal,RSB,type,N,f0,fe,N_v)
 
-Ps = sum(signal^2);
-
+Ps = sum(signal.^2);
 if strcmp (type,'tonal')
+    
 xtonal = sin(2*pi*f0/fe*N_v);
+Pb = sum(xtonal.^2);
+a = sqrt((Ps/Pb)*10^(RSB/10));
+signal = signal + a*xtonal ;
 
-Pb = sum(xtonal^2);
 end
 
-a = sqrt((Ps/Pb)*10^(RSB/10));
-signal = signal + a*xtonal;
+if strcmp(type, 'gaussien')
+    xgaussien = randn(1,N);
+    Pb = sum(xgaussien.^2);
+    a = sqrt((Ps/Pb)*10^(RSB/10));
+signal = signal + a*xgaussien;
+end
+
+
 
 
 end
